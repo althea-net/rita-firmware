89f29c959385198f79bfbf69a125250deae149da
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2018-04-02 15:48:26 -0400

    update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -2,18 +2,12 @@
 set -eux
 export SERVER=updates
 export HTTP_DIR=/usr/share/nginx/html/
-export TO_BUILD="edgerouterlite.yml edgerouterx.yml omnia.yml n750.yml"
-for config in $TO_BUILD; do
-	ansible-playbook -e @profiles/devices/$config -e @profiles/management/althea-dev.yml firmware-build.yml
-done
-
-
-for dir in build/bin/packages/
+for file in profiles/devices/* 
 do
-	scp -r $dir $SERVER:$HTTP_DIR
+	ansible-playbook -e @$file -e @profiles/management/althea-dev.yml firmware-build.yml
 done
 
-for dir in build/bin/targets/
-do
-	scp -r $dir $SERVER:$HTTP_DIR
-done
+
+rsync -ahz --delete build/bin/packages $SERVER:$HTTP_DIR/
+
+rsync -ahz --delete build/bin/targets $SERVER:$HTTP_DIR/
