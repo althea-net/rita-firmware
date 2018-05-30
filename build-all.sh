8b30b4bba1558b22c89b497f892577751930fab8
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2018-05-29 20:29:18 -0400

    Don't update packages by default from master
    Stupid one line commit, but I know if I don't make it I'll end up
    uploading master software to updates.altheamesh.com and then everyone
    on stable will pull it down that night.

--- build-all.sh
+++ build-all.sh
@@ -4,10 +4,10 @@ export SERVER=updates
 export HTTP_DIR=/usr/share/nginx/html/
 for file in profiles/devices/*
 do
-	ansible-playbook -e @$file -e @profiles/management/althea-dev.yml -e gateway=true firmware-build.yml
+	ansible-playbook -e @$file -e @profiles/management/althea-release.yml -e gateway=true firmware-build.yml
 done
 
 
-rsync -ahz --delete build/bin/packages $SERVER:$HTTP_DIR/
+#rsync -ahz --delete build/bin/packages $SERVER:$HTTP_DIR/
 
 rsync -ahz --delete build/bin/targets $SERVER:$HTTP_DIR/
