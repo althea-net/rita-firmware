1870ef82ae497a84c0d01fd05b6bc0dd29eb0c9a
Stan Drozd <drozdziak1@gmail.com>
2018-08-09 17:34:46 +0200

    Add new in-bulk build scripts
    This removes the pre-existing build-all.sh script. Here's what the new
    ones do:
    * build-all-dev-images.sh: builds in althea-dev.yml profile, doesn't
    upoad anywhere
    * build-nightly-and-upload.sh: builds in althea-release.yml profile,
    only uploads firmware images
    * build-release-and-upload.sh: builds in althea-release.yml profile,
    uploads both packages and firmware images.
    
    Note: All new scripts are directory aware now.

---
+++ scripts/build-nightly-and-upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eux
+cd $(dirname $0)/..
+export SERVER=updates
+export HTTP_DIR=/usr/share/nginx/html/
+for file in profiles/devices/*
+do
+	ansible-playbook -e @$file -e @profiles/management/althea-release.yml -e gateway=true firmware-build.yml
+done
+
+rsync -ahz --delete build/bin/targets $SERVER:$HTTP_DIR/
