00aa7ce109aed3d324b0e7adb5d7651cc7c012b9
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2018-10-15 13:26:19 -0400

    Add profile with logging

---
+++ scripts/build-all-test-images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eux
+cd $(dirname $0)/..
+export SERVER=updates
+export HTTP_DIR=/usr/share/nginx/html/
+for file in profiles/devices/*
+do
+	ansible-playbook -e @$file -e @profiles/management/althea-test.yml -e gateway=true firmware-build.yml
+done
