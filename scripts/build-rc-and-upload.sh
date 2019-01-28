a33ab33d730399c591773a0dab9779731ae736fd
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2019-01-28 11:02:36 -0500

    Reorganize readme and release process
    This pull request better represents what each set of images actually does
    and what data they may or may not collect.

--- scripts/build-rc-and-upload.sh
+++ scripts/build-rc-and-upload.sh
@@ -5,7 +5,8 @@ export SERVER=updates
 export HTTP_DIR=/usr/share/nginx/html/
 for file in profiles/devices/*
 do
-	ansible-playbook -e @$file -e @profiles/management/althea-dev.yml -e gateway=true firmware-build.yml
+	ansible-playbook -e @$file -e @profiles/management/althea-test.yml -e gateway=true firmware-build.yml
 done
 
 rsync -ahz --delete build/bin/packages $SERVER:$HTTP_DIR/rc/
+rsync -ahz --delete build/bin/targets $SERVER:$HTTP_DIR/
