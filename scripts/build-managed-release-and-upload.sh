e6bb234ef2061cba818865c23165f4d07d7173aa
Justin Kilpatrick <justin@althea.net>
2019-09-27 11:19:23 -0400

    Update build scripts
    We want to fail when one of the supported builds fails. Also the gateway
    parameter is long out of date by now and isn't being read anywhere.

--- scripts/build-managed-release-and-upload.sh
+++ scripts/build-managed-release-and-upload.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-set -ux
+set -eux
 cd $(dirname $0)/..
 export SERVER=updates
 export HTTP_DIR=/usr/share/nginx/html/
-for file in profiles/devices/*
-do
-	ansible-playbook -e @$file -e @profiles/management/althea-managed.yml -e gateway=true firmware-build.yml
-done
+ansible-playbook -e @profiles/devices/wrt3200acm.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+ansible-playbook -e @profiles/devices/wrt32x.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+ansible-playbook -e @profiles/devices/glb1300.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+ansible-playbook -e @profiles/devices/ea6350v3.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+ansible-playbook -e @profiles/devices/n600.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+ansible-playbook -e @profiles/devices/n750.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+ansible-playbook -e @profiles/devices/edgerouterx.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+ansible-playbook -e @profiles/devices/x86_64.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 
 rsync -ahz --delete build/bin/targets $SERVER:$HTTP_DIR/supported
