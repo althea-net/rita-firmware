080332e869e580a80cb24cb39f60a76425b13f33
Justin Kilpatrick <justin@althea.net>
2021-06-07 16:27:38 -0400

    Add support for ea7500v2 and ea7300v1
    I have not specifically tested either of these devices, but I'm guessing
    that since they are of the same cpu arch that they share most
    properties of the ea7300v2, I'm going to take the wild guess of building
    images for them and sending them to Stephen to test.

--- scripts/build-managed-release-and-upload.sh
+++ scripts/build-managed-release-and-upload.sh
@@ -13,9 +13,11 @@ ansible-playbook -e @profiles/devices/edgerouterx.yml -e @profiles/management/al
 ansible-playbook -e @profiles/devices/x86_64.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/wrt1900acs.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/wrt1900ac.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+ansible-playbook -e @profiles/devices/edgerouterx-sfp.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 
 # not quite supported but we want them to work
 ansible-playbook -e @profiles/devices/ea7300v2.yml -e @profiles/management/althea-managed.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/edgerouterx-sfp.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+ansible-playbook -e @profiles/devices/ea7300v1.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+ansible-playbook -e @profiles/devices/ea7500v2.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 
 rsync -ahz --delete build/bin/targets $SERVER:$HTTP_DIR/supported
