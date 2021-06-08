2c4fc68f389bea1ca48dde854b045f91c5f795a4
Justin Kilpatrick <justin@althea.net>
2021-06-08 15:13:12 -0400

    Classify devices in managed release build
    This both makes things a little more organized and provides more
    immediate images builds by clustering devices of the same time, which
    will share kernel and user space configuration.

--- scripts/build-managed-release-and-upload.sh
+++ scripts/build-managed-release-and-upload.sh
@@ -3,20 +3,29 @@ set -eux
 cd $(dirname $0)/..
 export SERVER=updates
 export HTTP_DIR=/usr/share/nginx/html/
+
+# desktops / servers
+ansible-playbook -e @profiles/devices/x86_64.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+
+# mvebu
 ansible-playbook -e @profiles/devices/wrt3200acm.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/wrt32x.yml -e @profiles/management/althea-managed.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/glb1300.yml -e @profiles/management/althea-managed.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/ea6350v3.yml -e @profiles/management/althea-managed.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/n750.yml -e @profiles/management/althea-managed.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/edgerouterx.yml -e @profiles/management/althea-managed.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/x86_64.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/wrt1900acs.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/wrt1900ac.yml -e @profiles/management/althea-managed.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/edgerouterx-sfp.yml -e @profiles/management/althea-managed.yml firmware-build.yml
-
 # not quite supported but we want them to work
 ansible-playbook -e @profiles/devices/ea7300v2.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/ea7300v1.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/ea7500v2.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+
+# ipq40xx
+ansible-playbook -e @profiles/devices/glb1300.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+ansible-playbook -e @profiles/devices/ea6350v3.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+
+# ath79
+ansible-playbook -e @profiles/devices/n750.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+
+# ramips
+ansible-playbook -e @profiles/devices/edgerouterx.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+ansible-playbook -e @profiles/devices/edgerouterx-sfp.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 
 rsync -ahz --delete build/bin/targets $SERVER:$HTTP_DIR/supported
