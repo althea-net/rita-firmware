b4e1edb00ddf354b443e14e7bd9122446797dc78
Justin Kilpatrick <justin@althea.net>
2021-06-08 12:01:41 -0400

    Fix supported image builds for OpenWRT 21.02 development
    This regenerates all supported device configs in order to provide
    compatibility with 21.02, it also removes the n600 target which is now
    no longer upstream. Finally it upgrades the n750 target to the ath79
    backend.
    
    This does not modify unsupported targets, which probably need attention
    as well to get working again.

--- scripts/build-managed-release-and-upload.sh
+++ scripts/build-managed-release-and-upload.sh
@@ -7,7 +7,6 @@ ansible-playbook -e @profiles/devices/wrt3200acm.yml -e @profiles/management/alt
 ansible-playbook -e @profiles/devices/wrt32x.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/glb1300.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/ea6350v3.yml -e @profiles/management/althea-managed.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/n600.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/n750.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/edgerouterx.yml -e @profiles/management/althea-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/x86_64.yml -e @profiles/management/althea-managed.yml firmware-build.yml
