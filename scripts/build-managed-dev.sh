205f4a490f1568545a7ca4bce5c73b2b7efedf60
Thomas Spellman <thomas@althea.systems>
2023-07-06 17:41:09 -0700

    update the dev build script for the current dev routers

--- scripts/build-managed-dev.sh
+++ scripts/build-managed-dev.sh
@@ -4,17 +4,17 @@ cd $(dirname $0)/..
 export SERVER=updates
 export HTTP_DIR=/usr/share/nginx/html/
 
-# ramips
-ansible-playbook -e @profiles/devices/cudy_wr2100.yml -e @profiles/management/althea-managed.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/mikrotik_routerboard-750gr3.yml -e @profiles/management/althea-managed.yml firmware-build.yml
-
-# rockchip
-ansible-playbook -e @profiles/devices/nanopi-r2s.yml -e @profiles/management/althea-managed.yml firmware-build.yml
-
-# mvebu
+# it's probably best to do: `cd build && make dirclean` 
 
 # ipq40xx
-ansible-playbook -e @profiles/devices/mikrotik_hap-ac2.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+ansible-playbook -e @profiles/devices/gl-b2200.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+
+# ipq80xx
+#ansible-playbook -e @profiles/devices/netgear_xr500.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+
+#rockchip
+#ansible-playbook -e @profiles/devices/linkstar_h68k.yml -e @profiles/management/althea-managed.yml firmware-build.yml
+
 
 
 # rsync -ahz --delete build/bin/targets $SERVER:$HTTP_DIR/supported
