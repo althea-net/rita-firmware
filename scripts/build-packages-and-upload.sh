4867cf387cc26eccfd59908fde829584cd2551c9
Justin Kilpatrick <justin@althea.net>
2023-08-02 18:34:08 -0400

    Do not copy images while building packages

--- scripts/build-packages-and-upload.sh
+++ scripts/build-packages-and-upload.sh
@@ -6,7 +6,7 @@ export SERVER=updates
 export HTTP_DIR=/usr/share/nginx/html/
 for file in profiles/devices/*
 do
-	ansible-playbook -e @$file -e @profiles/management/althea-release.yml firmware-build.yml
+	ansible-playbook -e @$file -e @profiles/management/althea-packages.yml firmware-build.yml
 done
 
 rsync -ahz --delete build/bin/packages $SERVER:$HTTP_DIR/rc/
