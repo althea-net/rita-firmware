c9cae9fa05870eb9bdeec7d313c7c37f89b70089
Justin Kilpatrick <justin@althea.net>
2023-10-23 15:53:20 -0400

    Remove mips targets from default builds
    Rust has downgraded support for the mips toolchain, generally making it
    much harder to compile with using modern rust versions. We don't really
    use any mips routers at this point so the quickest option is to drop
    support.
    
    End of an era

--- scripts/build-default-release-and-upload.sh
+++ scripts/build-default-release-and-upload.sh
@@ -4,16 +4,6 @@ cd $(dirname $0)/..
 export SERVER=updates
 export HTTP_DIR=/usr/share/nginx/html/
 
-# ramips
-ansible-playbook -e @profiles/devices/edgerouterx.yml -e @profiles/management/althea-release.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/edgerouterx-sfp.yml -e @profiles/management/althea-release.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/tplinka6v3.yml -e @profiles/management/althea-release.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/ea7300v2.yml -e @profiles/management/althea-release.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/ea7300v1.yml -e @profiles/management/althea-release.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/ea7500v2.yml -e @profiles/management/althea-release.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/cudy_wr2100.yml -e @profiles/management/althea-release.yml firmware-build.yml
-ansible-playbook -e @profiles/devices/linksys_e5600.yml -e @profiles/management/althea-release.yml firmware-build.yml
-
 # desktops / servers
 ansible-playbook -e @profiles/devices/x86_64.yml -e @profiles/management/althea-release.yml firmware-build.yml
 
@@ -36,8 +26,3 @@ ansible-playbook -e @profiles/devices/netgear_ex6100v2.yml -e @profiles/manageme
 
 #broadcom
 ansible-playbook -e @profiles/devices/pi4-64.yml -e @profiles/management/althea-release.yml firmware-build.yml
-
-# ath79
-ansible-playbook -e @profiles/devices/n750.yml -e @profiles/management/althea-release.yml firmware-build.yml
-
-rsync -ahz --delete build/bin/targets $SERVER:$HTTP_DIR/supported
