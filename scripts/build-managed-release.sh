bf64b2d028fb9b8e74086e555b60567996a67aae
Justin Kilpatrick <justin@althea.net>
2023-12-29 13:25:02 -0500

    Add dl-wrtx36 support
    This is an exciting new router with a lot of cpu power and 2.5gbit nics

--- scripts/build-managed-release.sh
+++ scripts/build-managed-release.sh
@@ -15,6 +15,9 @@ ansible-playbook -e @profiles/devices/wrt3200acm.yml -e @profiles/management/haw
 ansible-playbook -e @profiles/devices/wrt32x.yml -e @profiles/management/hawk-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/wrt1900acs.yml -e @profiles/management/hawk-managed.yml firmware-build.yml
 
+# ipq807x
+ansible-playbook -e @profiles/devices/dl-wrtx36.yml -e @profiles/management/hawk-managed.yml firmware-build.yml
+
 # ipq40xx
 ansible-playbook -e @profiles/devices/glb1300.yml -e @profiles/management/hawk-managed.yml firmware-build.yml
 ansible-playbook -e @profiles/devices/ea6350v3.yml -e @profiles/management/hawk-managed.yml firmware-build.yml
