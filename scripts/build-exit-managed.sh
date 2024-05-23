bd95460177fcae718d83dc3727aca99e534d9c7c
Justin Kilpatrick <justin@althea.net>
2024-05-23 16:46:26 -0400

    Update Exit firmware building
    Up until now this repo has supported building exit firmwares, but exit
    firmwares where not exceptionally useful by virtue of the design of the
    exit software itself. Since private information such as postgresql
    credentials had to be built into the image for it to be usable and no
    dashboard existed to assist in field configuration exit images while
    technically possible have only ever been built once or twice.
    
    This patch updates the firmware builder to reflect upstream changes in
    beta 22 of Rita Exit which make it entierly feasible to build out ship
    and forget exit firmwares that don't require any special configuration
    to be used in the field.

---
+++ scripts/build-exit-managed.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eux
+cd $(dirname $0)/..
+
+# desktops / servers
+ansible-playbook -e @profiles/exit/x86_64.yml -e @profiles/management/hawk-managed.yml firmware-build.yml
+
+ansible-playbook -e @profiles/exit/mikrotik_hap-ac3.yml -e @profiles/management/hawk-managed.yml firmware-build.yml
